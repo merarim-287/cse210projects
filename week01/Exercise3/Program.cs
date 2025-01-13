d3b56866fc6b52f6a43f214628e802412114de58
merari-287 <merari_287@outlook.com>
2025-01-13 11:52:34 -0700

    no comment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
+using System.Net;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = RandomNumberGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magicNumber)// While loop
+        {
+            Console.WriteLine("What is the magic number? ") ;
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+            }
+        }
     }
 }
