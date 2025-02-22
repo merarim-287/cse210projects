b473617f9681d46f5b0c9689313ef5f67984c100
merari-287 <merari_287@outlook.com>
2025-02-22 14:40:28 -0700

    goal.cs

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,30 @@
+public class IsComplete()
+{
+    public bool IsComplete()
+        public string _name { get; set; }
+        public string _description { get; set; }
+        public double _points { get; set; }
+        
+
+    public void RecordEvent()
+    {
+        
+    }
+
+    public bool IsComplete()
+    {
+
+    }
+
+    public string StringRepresentation
+    {
+        get
+        {
+            Console.readLine();
+            return _name;
+
+        }
+
+        
+    }
+}
