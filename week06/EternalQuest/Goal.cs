a431d6e90b1451b0498d94b65eab9b9bdf5a32c9
merari-287 <merari_287@outlook.com>
2025-02-21 18:44:14 -0700

    goal strings

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,31 @@
+public class Goal: GoalManager
+{
+    public string _shortName()
+        public string _goalName {get; set;}
+        public string _GoalDescription {get; set;}
+        public double _GoalPoints {get; set;}
+
+
+
+    public void RecordEvent()
+    {
+        
+    }
+
+
+    public bool IsComplete()
+    {
+        
+    }   
+
+    public string GetDetailsString()
+    {
+        
+    }
+
+    public string GetStringRepresentation()
+    {
+        
+    }
+}
+
